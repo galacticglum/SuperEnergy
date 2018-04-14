08a3ae96c1a8cdaee7dfe753b5ea7db8653a67d7
GalacticGlum <nerftactic09@gmail.com>
2018-04-14 13:06:16 -0400

    Working movement and walk animations.

---
+++ Assets/Game/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 smoothening;
+    [SerializeField]
+    private Vector2 velocity;
+    [SerializeField]
+    private Transform target;
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - target.position).normalized;
+        float offset = 0;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            offset = 7;
+        }
+
+        Vector2 targetOffset = target.position + target.right * 0.5f * offset;
+        Vector2 position = new Vector2(Mathf.SmoothDamp(transform.position.x, targetOffset.x + direction.x, ref velocity.x, smoothening.x),
+                                       Mathf.SmoothDamp(transform.position.y, targetOffset.y + direction.y, ref velocity.y, smoothening.y));
+
+        transform.position = new Vector3(position.x, position.y, transform.position.z);
+    }
+}
---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Locomotion Settings")]
+    [SerializeField]
+    private float speed = 7;
+
+    [Header("Animation")]
+    [SerializeField]
+    private Animator legsAnimator;
+
+    private Vector3 velocity;
+    private new Rigidbody2D rigidbody2D;
+    private Animator animator;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+
+        if (horizontalAxis == 0 && verticalAxis == 0)
+        {
+            animator.SetFloat("Move", 0);
+            legsAnimator.SetFloat("Move", 0);
+
+            velocity = Vector3.zero;
+        }
+        else
+        {
+            animator.SetFloat("Move", 1);
+            legsAnimator.SetFloat("Move", 1);
+
+            velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * speed;
+        }
+
+        rigidbody2D.velocity = velocity;
+    }
+}
