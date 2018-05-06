19aab013517ea527a74397a6f831938a848f8590
GalacticGlum <nerftactic09@gmail.com>
2018-05-06 08:45:03 -0400

    Pathfinding and enhanced AI.

--- Assets/Game/Scripts/Enemy.cs
+++ Assets/Game/Scripts/Enemy.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using Pathfinding;
+using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Enemy : MonoBehaviour
 {
     private const float Speed = 2;
+    private const float MaxWaypointDistance = 2;
+
     private static readonly Color HurtColourTint = new Color(1, 0.55f, 0.55f);
 
     private float health = 100;
@@ -11,22 +14,49 @@ public class Enemy : MonoBehaviour
     private PlayerController playerController;
     private float lastTakeDamageTime;
 
+    private AIPath aiPath;
+    private Seeker seeker;
+    private int currentWaypoint;
+
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingLayerName = "Player";
+        spriteRenderer.sortingOrder = 1;
+
+        aiPath = gameObject.AddComponent<AIPath>();
+        seeker = GetComponent<Seeker>();
+
+        aiPath.whenCloseToDestination = CloseToDestinationMode.Stop;
+        aiPath.maxSpeed = Speed;
+        aiPath.rotationIn2D = true;
+        aiPath.gravity = Vector3.zero;
+        aiPath.updateRotation = false;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (Time.time > lastTakeDamageTime + 0.05f && spriteRenderer.color != Color.white)
         {
             spriteRenderer.color = Color.white;
         }
+    }
 
-        Vector2 direction = playerController.transform.position - transform.position;
+    private void FixedUpdate()
+    {
+        aiPath.destination = playerController.transform.position;
+
+        Path path = seeker.GetCurrentPath();
+        if (currentWaypoint >= path.vectorPath.Count) return;
+
+        Vector2 direction = path.vectorPath[currentWaypoint] - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.position = Vector2.MoveTowards(transform.position, playerController.transform.position - transform.right, Speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < MaxWaypointDistance)
+        {
+            currentWaypoint++;
+        }
     }
 
     public void TakeDamage(float damage)
@@ -41,11 +71,13 @@ public class Enemy : MonoBehaviour
         lastTakeDamageTime = Time.time;
     }
 
-    public static void Create(GameObject prefab, Vector3 position, PlayerController playerController)
+    public static GameObject Create(GameObject prefab, Vector3 position, PlayerController playerController)
     {
         GameObject enemyGameObject = Instantiate(prefab, position, Quaternion.identity);
         Enemy enemyInstance = enemyGameObject.AddComponent<Enemy>();
         enemyInstance.playerController = playerController;
+
+        return enemyGameObject;
     }
 
 }
--- Assets/Game/Scripts/SpawnController.cs
+++ Assets/Game/Scripts/SpawnController.cs
@@ -1,25 +1,152 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
     [SerializeField]
     private PlayerController playerController;
     [SerializeField]
-    private GameObject enemyPrefab;
+    private GameObject[] spawnObjects;
     [SerializeField]
-    private Transform[] spawnPoints;
+    private float spawnRadius = 20;
+
+    private GameObject spawnParent;
+
+    private int currentWaveSize = 1;
+
     [SerializeField]
-    private float spawnCooldown = 20;
-    [SerializeField]
-    private float spawnChance = 0.6f;
-    private float lastSpawnTime;
-    private int spawnerIndex;
+    private float timeUntilNextSpawn = 1.0f;
+
+    private void Start()
+    {
+        spawnParent = new GameObject("Spawn_Parent");
+    }
 
     private void Update()
     {
-        if (!(Time.time > lastSpawnTime + spawnCooldown) || !(Random.value < spawnChance)) return;
+        timeUntilNextSpawn -= Time.deltaTime;
+        if (timeUntilNextSpawn > 0.0f)
+        {
+            return;
+        }
 
-        lastSpawnTime = Time.time;
-        Enemy.Create(enemyPrefab, spawnPoints[spawnerIndex++ % spawnPoints.Length].position, playerController);
+        int objectIndex = GetRandomSpawnObject();
+
+        Vector3 offset = Random.onUnitSphere;
+        offset.z = 0;
+        offset = offset.normalized * spawnRadius;
+
+        GameObject enemyGameObject = Enemy.Create(spawnObjects[objectIndex], transform.position + offset, playerController);
+        enemyGameObject.transform.SetParent(spawnParent.transform);
+
+        --currentWaveSize;
+
+        timeUntilNextSpawn = Random.Range(GetNewMinTime(), GetNewMaxTime());
+        currentWaveSize = Random.Range(GetNewMinWave(), GetNewMaxWave());
+    }
+
+    private static int GetRandomWeightedIndex(float[] weights)
+    {
+        float totalweight = weights.Sum();
+        float randomValue = Random.Range(0.0f, totalweight);
+
+        int index = 0;
+        float accumulator = 0.0f;
+        while (index < weights.Length)
+        {
+            accumulator += weights[index];
+            if (accumulator >= randomValue)
+            {
+                return index;
+            }
+
+            ++index;
+        }
+
+        return weights.Length - 1;
+    }
+
+    private int GetRandomSpawnObject()
+    {
+        float[] objectWeights;
+        if (Time.time < 20.0f)
+        {
+            objectWeights = new[] { 1.0f };
+        }
+        else if (Time.time < 60.0f)
+        {
+            objectWeights = new[] { 1.0f };
+        }
+        else
+        {
+            objectWeights = new[] { 1.0f };
+        }
+
+        return Mathf.Clamp(GetRandomWeightedIndex(objectWeights), 0, spawnObjects.Length - 1);
+    }
+
+    private float GetNewMinTime()
+    {
+        if (currentWaveSize > 0)
+        {
+            return 1.0f;
+        }
+
+        if (Time.time < 5.0f)
+        {
+            return 3.0f;
+        }
+
+        return Time.time < 20.0f ? 6.0f : 8.0f;
+    }
+
+    private float GetNewMaxTime()
+    {
+        if (currentWaveSize > 0)
+        {
+            return 3.0f;
+        }
+
+        if (Time.time < 5.0f)
+        {
+            return 6.0f;
+        }
+
+        return Time.time < 20.0f ? 10.0f : 12.0f;
+    }
+
+    private int GetNewMinWave()
+    {
+        if (currentWaveSize > 0)
+        {
+            return currentWaveSize;
+        }
+
+        if (Time.time < 5.0f)
+        {
+            return 1;
+        }
+
+        return Time.time < 20.0f ? 2 : 3;
+    }
+
+    private int GetNewMaxWave()
+    {
+        if (currentWaveSize > 0)
+        {
+            return currentWaveSize;
+        }
+
+        if (Time.time < 5.0f)
+        {
+            return 5;
+        }
+
+        if (Time.time < 20.0f)
+        {
+            return 6;
+        }
+
+        return Time.time < 30.0f ? 8 : 12;
     }
 }
